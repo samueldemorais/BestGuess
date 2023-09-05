18f6d07c95ec7481291468ce67726e3a77a22d1b
Samuel de Morais <ssamueldemorais@gmail.com>
2023-09-05 01:11:03 -0300

    update interface and logic

--- app/src/main/java/samuel/morais/bestguess/MainActivity.kt
+++ app/src/main/java/samuel/morais/bestguess/MainActivity.kt
@@ -2,8 +2,10 @@ package samuel.morais.bestguess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +14,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var status: TextView
     private lateinit var answerUser: TextInputEditText
     private lateinit var answerButton: Button
+
+    private fun restartGame(guess: Guess): Guess {
+        smallNumber.text = "1"
+        higherNumber.text = "100"
+        val newGuess = Guess(smallNumber.text.toString().toInt(), higherNumber.text.toString().toInt())
+        Toast.makeText(this, "Jogo reiniciado!", Toast.LENGTH_SHORT).show()
+        return newGuess
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,25 +36,42 @@ class MainActivity : AppCompatActivity() {
         smallNumber.text = "1"
         higherNumber.text = "100"
 
-        val guess = Guess(smallNumber.text.toString().toInt(), higherNumber.text.toString().toInt())
+        var guess = Guess(smallNumber.text.toString().toInt(), higherNumber.text.toString().toInt())
+
 
         this.answerButton.setOnClickListener{
             var answerText = answerUser.text.toString().toInt()
+            answerUser.text = null
             if (guess.checkAnswer(answerText)){
                 status.text = "Parabéns! Você acertou"
-                //Toast.makeText(this, "Você perdeu!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Você ganhou!", Toast.LENGTH_SHORT).show()
             }
             else if(guess.checkRange(answerText)){
                 guess.changeRange(answerText)
                 smallNumber.text = guess.smallNumber.toString()
                 higherNumber.text = guess.higherNumber.toString()
+                if(guess.checkEqual()){
+                    status.text = "Perdeu! intervalos iguais"
+                    Toast.makeText(this, "Perdeu! intervalos iguais", Toast.LENGTH_SHORT).show()
+                    guess = restartGame(guess)
+                }
+                else{status.text = "Continue tentando"
+                Toast.makeText(this, "Continue tentando", Toast.LENGTH_SHORT).show()}
             }
-            else if(guess.checkEqual()){
-                status.text = "Perdeu! intervalos iguais"
-            }
-            else {status.text = "Perdeu! Número fora do intervalo"}
+            else {status.text = "Perdeu! Número fora do intervalo"
+                Toast.makeText(this, "Perdeu! Número fora do intervalo", Toast.LENGTH_SHORT).show()
+                guess = restartGame(guess)}
 
         }
 
+        this.status.setOnLongClickListener(View.OnLongClickListener {
+            guess = restartGame(guess)
+            true
+        })
+
     }
+
+
+
+
 }
